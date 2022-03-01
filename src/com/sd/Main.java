30a369340a27cb6f9129d2423150d22c31c5f49c
Nickolai <mailtilatloggeind@protonmail.com>
2022-03-01 22:20:30 +0100

    anden commit

--- src/com/sd/Main.java
+++ src/com/sd/Main.java
@@ -3,6 +3,6 @@ package com.sd;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
--- src/com/sd/Shape.java
+++ src/com/sd/Shape.java
@@ -1,4 +1,35 @@
 package com.sd;
 
-public class Shape {
+import java.util.ArrayList;
+
+public abstract class Shape {
+
+    ArrayList<Integer> Xkor = new ArrayList<Integer>();
+    ArrayList<Integer> Ykor = new ArrayList<Integer>();
+
+    Shape(){}
+
+    public int findMidterpunktX(){
+        return 0;
+    }
+
+    public int findMidterpunktY(){
+        return 0;
+    }
+
+    public double findAreal(){
+        return 0;
+    }
+
+    public double findOmkreds(){
+        return 0;
+    }
+
+    public boolean erPunktetIndeIFormen(){
+        return false;
+    }
+
+    public double afstandMellemFormer(){
+        return 0;
+    }
 }
