c3f2b93ef66b645a0f149a9c82d3107bf163b9e6
Nickolai <mailtilatloggeind@protonmail.com>
2022-03-02 23:59:57 +0100

    så virker rektangel

--- src/com/sd/Main.java
+++ src/com/sd/Main.java
@@ -11,8 +11,7 @@ public class Main {
         System.out.println("*** simpelRektangel: ***");
         System.out.println("midterpunktet er: " + simpelRektangel.findMidterpunkt());
         System.out.println("arealet er: " + simpelRektangel.findAreal());
-        System.out.println("omkredsen er: " + simpelRektangel.findOmkreds());
-        System.out.println("er punktet inden i formen?: " + simpelRektangel.erPunktetIndeIFormen() + "\n");
+        System.out.println("omkredsen er: " + simpelRektangel.findOmkreds() + "\n");
 
         //Opsætning og test af Rektangel + punkt
         Rektangel rektangelPunkt= new Rektangel(0, 1, 0,8, 6,8, 6, 1, 5, 3);
@@ -21,5 +20,15 @@ public class Main {
         System.out.println("arealet er: " + rektangelPunkt.findAreal());
         System.out.println("omkredsen er: " + rektangelPunkt.findOmkreds());
         System.out.println("er punktet inden i formen?: " + rektangelPunkt.erPunktetIndeIFormen() + "\n");
+
+        //Opsætning og test af 2 rektangler
+        Rektangel rektangler= new Rektangel(0, 1, 0,8, 6,8, 6, 1, 9, 1,
+                9, 8, 15, 8, 15, 1);
+        System.out.println("*** rektangler (to forskellige): ***");
+        System.out.println("midterpunktet af første rektangel er: " + rektangler.findMidterpunkt());
+        System.out.println("arealet af første rektangel er: " + rektangler.findAreal());
+        System.out.println("omkredsen af første rektangel er: " + rektangler.findOmkreds());
+        System.out.println("afstanden mellem midterpunkterne er: " + rektangler.afstandMellemFormer() + "\n");
+
     }
 }
--- src/com/sd/Rektangel.java
+++ src/com/sd/Rektangel.java
@@ -3,6 +3,8 @@ package com.sd;
 import java.awt.geom.Point2D;
 import java.util.Arrays;
 
+import static java.awt.geom.Point2D.distance;
+
 public class Rektangel extends Shape {
 
     //Opret doubles til at holde alle koordinator enkeltvis
@@ -44,6 +46,17 @@ public class Rektangel extends Shape {
 
     double midtX;
     double midtY;
+
+    double andenHøjX;
+    double andenLavX;
+    double andenHøjY;
+    double andenLavY;
+
+    double andenLængdeX;
+    double andenLængdeY;
+
+    double andenMidtX;
+    double andenMidtY;
 
     //Vi bruger overloaded constructors så man f.eks. ikke behøver at indtaste koordinator for 2 rektangler, hvis man bare vil have
     //arealet af ét
@@ -153,7 +166,59 @@ public class Rektangel extends Shape {
     }
 
     double afstandMellemFormer() {
+        // *** find midterpunkt for rektangel og opret et point2D.double med koordinatorne ***
 
-        return 0;
+        //Opret 2 arrays der holder henholdsvis X og Y koordinator,
+        //så de nemt kan sammenlignes
+        double[] Xkor = {AX, BX, CX, DX};
+        double[] Ykor = {AY, BY, CY, DY};
+
+        //Find ud af hvor lange de horisontale og vertikale sider er (virker kun i et rektangel fordi alle vinkler
+        // er 90 grader) og find herefter ud af hvor langt der er til midtpunktet af
+        Arrays.sort(Xkor); //Sorter Y koordinatorne, så vi kan udplukke den højeste og laveste
+        højX = Xkor[3];
+        lavX = Xkor[0];
+        midtX = (højX-lavX)/2;
+
+        Arrays.sort(Ykor); //Sorter Y koordinatorne, så vi kan udplukke den højeste og laveste
+        højY = Ykor[3];
+        lavY = Ykor[0];
+        midtY = (højY-lavY)/2;
+
+        //Lav et punkt ud fra X og Y koorndinatet og return det
+        //Vi pludser længden til midtpunktet med det laveste koordinat, så vi får midtpunktets koordinat
+        //Point2D.Double er ligesom et point altså to koordinator, men koordinatorne kan være doubles ikke kun ints
+        Point2D.Double midtPunkt = new Point2D.Double(midtX+lavX, midtY+lavY);
+        System.out.println("midtpunktet1 er: " + midtPunkt);
+
+        // *** find midterpunkt for rektangel 2 og opret et point2D.double med koordinatorne ***
+        //Opret 2 arrays der holder henholdsvis X og Y koordinator,
+        //så de nemt kan sammenlignes
+        double[] andenXkor = {andenAX, andenBX, andenCX, andenDX};
+        double[] andenYkor = {andenAY, andenBY, andenCY, andenDY};
+
+        //Find ud af hvor lange de horisontale og vertikale sider er (virker kun i et rektangel fordi alle vinkler
+        // er 90 grader) og find herefter ud af hvor langt der er til midtpunktet af
+        Arrays.sort(andenXkor); //Sorter Y koordinatorne, så vi kan udplukke den højeste og laveste
+        andenHøjX = andenXkor[3];
+        andenLavX = andenXkor[0];
+        andenMidtX = (andenHøjX-andenLavX)/2;
+
+        Arrays.sort(andenYkor); //Sorter Y koordinatorne, så vi kan udplukke den højeste og laveste
+        andenHøjY = andenYkor[3];
+        andenLavY = andenYkor[0];
+        andenMidtY = (andenHøjY-andenLavY)/2;
+
+        //Lav et punkt ud fra X og Y koorndinatet og return det
+        //Vi pludser længden til midtpunktet med det laveste koordinat, så vi får midtpunktets koordinat
+        //Point2D.Double er ligesom et point altså to koordinator, men koordinatorne kan være doubles ikke kun ints
+        Point2D.Double andenMidtPunkt = new Point2D.Double(andenMidtX+andenLavX, andenMidtY+andenLavY);
+        System.out.println("midtpunktet2 er: " + andenMidtPunkt);
+
+        // *** brug Distance til at finde afstanden mellem dem ***
+
+        double afstand = midtPunkt.distance(andenMidtPunkt.getX(), andenMidtPunkt.getY());
+
+        return afstand;
     }
 }
